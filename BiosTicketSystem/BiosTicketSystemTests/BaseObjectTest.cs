31fae144bdc1b6ed06327146c6003fb2f1a8e632
craftio <Kurtje17>
2022-02-03 16:26:14 +0100

    Test startup & merge

---
+++ BiosTicketSystem/BiosTicketSystemTests/BaseObjectTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BiosTicketSystemTests
+{
+    [TestClass]
+    public class BaseObjectTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

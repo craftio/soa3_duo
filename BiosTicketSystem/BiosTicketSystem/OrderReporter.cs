fd1fba378890ca7455b4d405896d68c058c5f9d8
myusernameistoolong <jbavdwal@live.nl>
2022-02-16 21:05:19 +0100

    Observable van order (WIP)

---
+++ BiosTicketSystem/BiosTicketSystem/OrderReporter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiosTicketSystem
+{
+    public class OrderReporter : IObservable<Order>
+    {
+        private List<IObserver<Order>> observers;
+
+        public OrderReporter()
+        {
+            observers = new List<IObserver<Order>>();
+        }
+
+        public IDisposable Subscribe(IObserver<Order> observer)
+        {
+            if (!observers.Contains(observer))
+                observers.Add(observer);
+            return new Unsubscriber(observers, observer);
+        }
+    }
+
+    class Unsubscriber : IDisposable
+    {
+        private List<IObserver<Order>> _observers;
+        private IObserver<Order> _observer;
+
+        public Unsubscriber(List<IObserver<Order>> observers, IObserver<Order> observer)
+        {
+            this._observers = observers;
+            this._observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+
+}

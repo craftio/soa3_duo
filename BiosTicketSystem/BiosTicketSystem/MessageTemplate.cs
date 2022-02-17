063200bd13a2a371cd078496c4aee7dbcddd4535
myusernameistoolong <jbavdwal@live.nl>
2022-02-17 15:36:54 +0100

    Create MessageTemplate.cs

---
+++ BiosTicketSystem/BiosTicketSystem/MessageTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiosTicketSystem
+{
+    public abstract class MessageTemplate
+    {
+        protected MessageTemplate() //final =/ sealed in C#?
+        {
+            Intro();
+            Content();
+            CopyRight();
+            End();
+        }
+
+        public abstract void Intro();
+
+        public void Content() { }
+
+        public void CopyRight()
+        {
+            Console.WriteLine("Copyright");
+        }
+
+        public void End()
+        {
+            Console.WriteLine("END");
+        }
+    }
+}
